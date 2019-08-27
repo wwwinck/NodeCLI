711d9fa6295886bec7e84d94ec253f1994c8cc17
Myr <myr@MacBook-Pro-de-Myr.local>
2019-08-27 09:50:47 +0200

    Beta

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 #!/usr/bin/env node
-console.log("Hello!");
+const countryList = require('country-list');
+const axios = require('axios');
+const api = 'https://date.nager.at/api/v2/PublicHolidays/';
+var date = new Date();
+var year = date.getFullYear();
+const args = process.argv;
+var country = args[2];
+const { getCode } = countryList;
+const countryCode = getCode(country);
+
+console.log()
+console.log()
+
+const holidates = async () => {
+    try {
+        const res = await axios.get(`${api}${year}/${countryCode}`)
+        var data = res.data
+        console.log()
+        console.log()
+        console.log('Here is the list of holidays in '+ country + ':');
+        console.log()
+        data.forEach(element =>
+            {
+                console.log('Date : ' + element.date + '   ' +  'Name : ' + element.name);
+            });
+            console.log()
+            console.log()
+        console.log('There is actually ' + data.length + ' holidays in '+ country);
+        console.log()
+        console.log()
+    } catch (err)
+        {
+            console.error(err);
+        }
+};
+holidates();
